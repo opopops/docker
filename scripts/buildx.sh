6067517412b02055cea108d20ac9d1a2feb25747
Germain GUEUTIER <lemaildegermain@gmail.com>
2024-04-04 15:29:03 +0200

    improve buildx script

--- scripts/buildx.sh
+++ scripts/buildx.sh
@@ -8,9 +8,9 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 BUILDX_ARGS=""
 BUILDX_PRUNE="false"
 BUILDX_BUILDER_NAME="${BUILDX_BUILDER_NAME:-opopops}"
-BUILDX_BUILDER_PLATFORM="${BUILDX_BUILDER_PLATFORM:-linux/amd64,linux/arm64}"
 BUILDX_PRUNE_FILTER="${BUILDX_PRUNE_FILTER:-until=72h}"
 BUILDX_PRUNE_KEEP_STORAGE="${BUILDX_PRUNE_KEEP_STORAGE:-5gb}"
+
 
 function _exit() {
   if [[ "$BUILDX_PRUNE" == "true" ]]; then
@@ -35,8 +35,14 @@ OPTIONS:
     --builder <string>
         Override the configured builder instance (default "$BUILDX_BUILDER_NAME")
 
+    --build-arg <key=value>
+        Set build args
+
     --platform <string>
         Set target platform for build (format: "linux/amd64,linux/arm64")
+
+    --tag
+        Set image tag
 
     --load
         Load image
@@ -61,13 +67,13 @@ while (("$#")); do
     BUILDX_BUILDER_NAME="$2"
     shift 2
     ;;
+  --build-arg | --platform | --tag)
+    BUILDX_ARGS+=" $1=$2"
+    shift 2
+    ;;
   --load | --push)
     BUILDX_ARGS+=" $1"
     shift
-    ;;
-  --platform | --tag)
-    BUILDX_ARGS+=" $1=$2"
-    shift 2
     ;;
   --prune)
     BUILDX_PRUNE="true"
@@ -85,9 +91,13 @@ while (("$#")); do
   esac
 done
 
+if [[ -z "$BUILDX_CONTEXT" ]]; then
+  echo "ERROR: you must specify a context"
+  exit 1
+fi
+
 set +e
 docker buildx create \
-  --platform="$BUILDX_BUILDER_PLATFORM" \
   --name="$BUILDX_BUILDER_NAME" 2>/dev/null
 set -e
 
